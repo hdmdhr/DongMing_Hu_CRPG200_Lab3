52401c96093fe67796bb27f95af655a7a6854df7
DongMing <hdmdhr@gmail.com>
2019-03-10 00:13:35 -0700

    Combobox doesn't show ID number.

--- DongMing_Hu_CRPG200_Lab3/Program.cs
+++ DongMing_Hu_CRPG200_Lab3/Program.cs
@@ -16,7 +16,7 @@ namespace DongMing_Hu_CRPG200_Lab3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmNorthwind());
         }
     }
 }
---
+++ DongMing_Hu_CRPG200_Lab3/frmNorthwind.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DongMing_Hu_CRPG200_Lab3
+{
+    public partial class frmNorthwind : Form
+    {
+        public frmNorthwind()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void frmNorthwind_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Order_Details' table. You can move, or remove it, as needed.
+            this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
+            // TODO: This line of code loads data into the 'northwindDataSet.Categories' table. You can move, or remove it, as needed.
+            this.categoriesTableAdapter.Fill(this.northwindDataSet.Categories);
+            // TODO: This line of code loads data into the 'northwindDataSet.Suppliers' table. You can move, or remove it, as needed.
+            this.suppliersTableAdapter.Fill(this.northwindDataSet.Suppliers);
+            // TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+        }
+    }
+}
