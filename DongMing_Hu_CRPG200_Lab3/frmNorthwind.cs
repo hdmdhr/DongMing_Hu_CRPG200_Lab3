d7755ce702e4e500f77218165711d53c30616bde
DongMing <hdmdhr@gmail.com>
2019-03-10 19:04:33 -0600

    Add try catch caluses to handle errors. Combobox still doesn't show ID# on home pc.

--- DongMing_Hu_CRPG200_Lab3/frmNorthwind.cs
+++ DongMing_Hu_CRPG200_Lab3/frmNorthwind.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,22 +21,49 @@ namespace DongMing_Hu_CRPG200_Lab3
 
         private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
-            this.Validate();
-            this.productsBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+            try
+            {
+                this.Validate();
+                this.productsBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+            }
+            catch (DBConcurrencyException)
+            {
+
+                MessageBox.Show("Some rows were not updated due concurrency error, please try again.", "Concurrency Exception");
+                this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
+                this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+            }
+            catch (DataException ex)
+            {
+                // include ConstraintEx, NoNullAllowedEx
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                productsBindingSource.CancelEdit();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database error # " + ex.Number + ": " + ex.Message, ex.GetType().ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Other error while saving data: " + ex.Message, "Others Error");
+            }
 
         }
 
         private void frmNorthwind_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'northwindDataSet.Order_Details' table. You can move, or remove it, as needed.
-            this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
-            // TODO: This line of code loads data into the 'northwindDataSet.Categories' table. You can move, or remove it, as needed.
-            this.categoriesTableAdapter.Fill(this.northwindDataSet.Categories);
-            // TODO: This line of code loads data into the 'northwindDataSet.Suppliers' table. You can move, or remove it, as needed.
-            this.suppliersTableAdapter.Fill(this.northwindDataSet.Suppliers);
-            // TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
-            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+            try
+            {
+                this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
+                this.categoriesTableAdapter.Fill(this.northwindDataSet.Categories);
+                this.suppliersTableAdapter.Fill(this.northwindDataSet.Suppliers);
+                this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database load error # "+ex.Number+": "+ex.Message, ex.GetType().ToString());
+            }
 
         }
     }
